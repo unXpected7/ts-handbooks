e5fbd669abacf03a02b80a0bb3520148206d499c
unXpected7 <ilyasyusuf002@gmail.com>
2022-04-02 17:14:57 +0700

    parent product

---
+++ module/BaseLaptop.ts
@@ -0,0 +1,31 @@
+import ILaptop from "./ILaptop";
+import * as Keyboard from "./Keyboard";
+import {b} from "./Keyboard";
+
+abstract class Baselaptop<T> implements ILaptop<T> {
+    name: string;
+    type: T;
+    withNumeric: boolean;
+    withTouchButton: boolean;
+
+    constructor(
+        name: string,
+        type: T,
+        numeric: boolean,
+        touchButton: boolean) {
+            this.name = name;
+            this.type = type;
+            this.withNumeric = numeric;
+            this.withTouchButton = touchButton;
+    }
+
+    a(): void {
+        console.log(Keyboard.a());
+    }
+
+    b():void {
+        console.log( b());
+    }
+}
+
+export default Baselaptop;
