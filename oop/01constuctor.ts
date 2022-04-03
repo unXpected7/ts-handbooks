413cbbfd52407ad618b586c4e36eec0d2612c624
unXpected7 <ilyasyusuf002@gmail.com>
2022-04-03 16:03:58 +0700

    constuctor

---
+++ oop/01constuctor.ts
@@ -0,0 +1,8 @@
+//? constructor is method used for object/class
+class Animal1 {
+    constructor() {
+        console.log('constuctor will always run earlier');
+    }
+}
+
+const animal1 = new Animal1();
